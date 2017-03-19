2d17f20a0f8fb6042968aec689318e9fe6e51576
CloudGuan <kirito291305463@gmail.com>
2017-03-19 22:24:46 +0800

    add： compute ray intersect with triangle

--- RacTracing/Core/Mesh.cpp
+++ RacTracing/Core/Mesh.cpp
@@ -19,21 +19,38 @@ IntersectResult StaticMesh::Intersect(RayTracing::Ray&)
 bool StaticMesh::IntersectTriangle(const SVertex& InV0, const SVertex& InV1, const SVertex& InV2, const RayTracing::Ray& InRay)
 {
 	/**Compute edges of triangle and normal */
-	
-    RayTracing::Vector3 mEdge0 = InV1.mPosition - InV0.mPosition;
+
+	RayTracing::Vector3 mEdge0 = InV1.mPosition - InV0.mPosition;
 	RayTracing::Vector3 mEdge1 = InV2.mPosition - InV0.mPosition;
-	RayTracing::Vector3 mNormal = mEdge0.cross(mEdge1);
+	//RayTracing::Vector3 mNormal = mEdge0.cross(mEdge1);
 	/**
 	* if parrel to plane of triangle
-	* greater zero means intersect with triangle 
+	* greater zero means intersect with triangle
 	*/
-	if (InRay.GetDirection().dot(mNormal) == 0)
+	RayTracing::Vector3 mDxE2 = InRay.GetDirection().cross(mEdge1);
+	double mDet = mDxE2.dot(mEdge1);
+	/**if det is negative,intersect with the backface*/
+	if (mDet < 0)
 		return false;
+
 	/**
 	* the experience :  P = (1-u-v)A-uB-vC;
 	* if P is inside of Triangle, 0<=u<=1 0<=v<=1 and u+v<=1
 	*/
+
+	double mInvDet = 1.0 / mDet;
+	RayTracing::Vector3 mOsubA = InRay.GetOrigin() - InV0.mPosition;
+
+	double mU = mOsubA.dot(mDxE2)*mInvDet;
+	if (mU < 0 || mU>1) return false;
+
+	RayTracing::Vector3 mTxE1 = mOsubA.cross(mEdge0);
+	double mV = InRay.GetDirection().dot(mTxE1)*mInvDet;
+
+	if (mV < 0 || (mV + mU)>1) return false;
+
+	double mT = mTxE1.dot(mEdge1)*mInvDet;
 	
-	return false;
+	return true;
 }
 
